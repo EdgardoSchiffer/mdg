f0dacaa9b8da6ebbe0d0401bd477d3b18b218566
EdgardoSchiffer <EdgardoSchiffer@users.noreply.github.com>
2015-03-09 08:44:44 -0600

    Create CalcularPrecio.php

---
+++ php/CalcularPrecio.php
@@ -0,0 +1,13 @@
+<?php 
+if (!empty($_GET)) {
+	$id = $_REQUEST["idCo"];
+	require_once("conexion.php");
+	$obj = new Conexion();
+	$pst = $obj->conexion->prepare("SELECT  (p.precio * SUM(s.cantidad))   FROM  platillos p join servicio s  on p.id_platillo = s.id_platillo   WHERE id_reservacion= :idevento");
+ 	$pst->bindValue(":idevento", $id);
+	$pst->execute();
+	$result = $pst->fetch();
+	echo '{"total" : "'.$result[0].'"}';
+	$obj->conexion = null;
+}
+?>
